0914d0cd1d6a202923fd3d9dee0f2373d571514d
zhisuocao <zhong.85@osu.edu>
2016-10-29 14:35:54 -0400

    coupon tried

--- js/custom.js
+++ js/custom.js
@@ -368,7 +368,33 @@ jQuery(function($){
 		cssEase: 'linear'
     });
 
+      /* ----------------------------------------------------------- */
+  /*  15. POP UP COUPON
+  /* ----------------------------------------------------------- */   
+    // Get the coupon
+    var coupon = document.getElementById('coupons');
 
-    
+    // Get the button that opens the coupon
+    var btn = document.getElementById("viewCoupon");
+
+    // Get the <span> element that closes the coupon
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the coupon 
+    btn.onclick = function() {
+        coupon.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the coupon
+    span.onclick = function() {
+        coupon.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the coupon, close it
+    window.onclick = function(event) {
+        if (event.target == coupon) {
+            coupon.style.display = "block";
+        }
+    }  
 });
 
