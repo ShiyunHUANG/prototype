31aea405d6ed3863b3d3e10ff86c5f02be4c16d8
Shu Wang <shuwang@andrew.cmu.edu>
2016-10-30 21:24:11 -0400

    updated

--- js/custom.js
+++ js/custom.js
@@ -367,34 +367,17 @@ jQuery(function($){
 		cssEase: 'linear'
     });
 
-      /* ----------------------------------------------------------- */
+  /* ----------------------------------------------------------- */
   /*  15. POP UP COUPON
-  /* ----------------------------------------------------------- */   
-    // Get the coupon
-    var coupon = document.getElementById('coupons');
+  /* ----------------------------------------------------------- */  
 
-    // Get the button that opens the coupon
-    var btn = document.getElementById("viewCoupon");
+	jQuery("#viewCoupon").click(function(e){
+		$('#coupons').attr('style', 'display: block');
+	});
 
-    // Get the <span> element that closes the coupon
-    var span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks on the button, open the coupon 
-    btn.onclick = function() {
-        coupon.style.display = "block";
-    }
-
-    // When the user clicks on <span> (x), close the coupon
-    span.onclick = function() {
-        coupon.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the coupon, close it
-    window.onclick = function(event) {
-        if (event.target == coupon) {
-            coupon.style.display = "block";
-        }
-    } 
-    
+	// When the user clicks on <span> (x), close the coupon
+	jQuery(".close").click(function(e){
+		$('#coupons').attr('style', 'display: none');
+	});
 });
 
